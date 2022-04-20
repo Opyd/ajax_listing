68dfb606a0abc1fed45d531bf7f6489c837bea94
Daniel <danielopyd99@gmail.com>
2022-04-20 15:25:33 +0200

    sorting, csv's json and xml parser, dummy data

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import pkg from "body-parser";
+const { json, urlencoded } = pkg;
+import loadCSVXML from "./loadCSVXML.js";
+import path from "path";
+import { fileURLToPath } from "url";
+const app = express();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "..", "views")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/test", (req, res) => {
+  res.send({ xd: "dupsko" });
+});
+
+app.get("/loadAll", async (req, res) => {
+  res.set("content-type", "text/xml");
+  res.send(await loadCSVXML());
+});
+
+app.listen(8080, async () => {
+  console.log("serwer działa");
+});
---
+++ src/loadCSVXML.js
@@ -0,0 +1,36 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import csv from "fast-csv";
+
+export default async function exportCSV() {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  var queryParameter = () =>
+    new Promise((resolve) => {
+      let returnLit = [];
+      csv
+        .parseFile(path.resolve(__dirname, "data", "cars_data.csv"), {
+          headers: true,
+        })
+        .on("data", (data) => {
+          returnLit.push(data);
+        })
+        .on("end", () => {
+          resolve(returnLit);
+        });
+    });
+  let mainList = [];
+  await queryParameter().then((res) => (mainList = res));
+
+  mainList = mainList.sort((a, b) => (a.cena < b.cena ? 1 : -1));
+
+  let xmled = '<?xml version="1.0" encoding="UTF-8"?>\n<CATALOG>\n';
+  mainList.forEach((val, key) => {
+    xmled += "<car>\n";
+    Object.keys(val).forEach((key) => {
+      xmled += `\t<${key}>${val[key]}</${key}>\n`;
+    });
+    xmled += "</car>\n";
+  });
+  xmled += "</CATALOG>";
+  return xmled;
+}
